2e3741876e3a2a82243bd770dc6440ac62adbc07
unknown <hello@ahmedzougari.com>
2023-04-05 23:47:39 +0100

    start working on destantion page

--- apps/space-tourism-mutli-page-website/src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-export const Button = () => {
-  return <button>go</button>
-}
-
-// before:content-[""] bg-white before:absolute before:rounded-full before:-z-[1] before:bg-[red] z-10 isolate relative before:w-[164%] before:aspect-square uppercase font-bellefair tracking-[1.25px] leading-[23px] mx-auto text-xl w-[150px] aspect-square rounded-full flex items-center justify-center'
-
-export default Button
